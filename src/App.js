563b35cdc17e47d73e3dbf9b864bea0185e54552
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-08-23 19:46:09 +0700

    Add main components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return ( 
+    <div className = "App" >
+        <Sidebar></Sidebar>
+        <Main></Main>
+        <Player></Player>
     </div>
-  );
+    );
+}
+
+
+function Main() {
+    return ( <div className = "Main"> Главная панель </div>
+    )
+}
+
+
+function Sidebar() {
+    return (<div className = "Sidebar"> Боковая панель </div>
+    )
+}
+
+
+function Player() {
+    return <div className = "Player"> Плеер </div>
 }
 
 export default App;
